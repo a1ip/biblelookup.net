befbc55cfdfcfb12b4f4142e1289bfe43a2417c2
Phil Rigovanov <phil@rigovanov.ru>
2011-05-16 23:25:54 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiblelookupNet::Application.config.secret_token = 'd774b264b77be48b0cc22ad5ddb622595f96ab6a3489395cbf603aa8223b2e862338b07a4d30d14a67536617746afc1d63a6dd7d1794e90cf528f6332323a372'
